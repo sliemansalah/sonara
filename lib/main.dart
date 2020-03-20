5d965fdcb704df1dc38a024b4e3e66f65f1570ab
slieman <slieman2001@hotmail.com>
2020-03-21 01:40:21 +0200

    convert to arabic language.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,38 @@
 import 'package:flutter/material.dart';
-
 import './screens/main_drawer.dart';
+
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
 
-void _healing(){
+void _healing() {
   print('healing');
 }
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale("ar"),
+        ],
+        locale: Locale("ar"),
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            backgroundColor: Colors.red,
-            title: Text('sonara.net',
-                style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                )),
-          ),
-          drawer: MainDrawer()
-        ));
+            appBar: AppBar(
+              centerTitle: true,
+              backgroundColor: Colors.red,
+              title: Text('sonara.net',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+            drawer: MainDrawer()));
   }
 }
