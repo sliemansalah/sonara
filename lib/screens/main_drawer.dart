3afcf9484a65fa6f468af9cd1b4222e5df319de0
slieman <slieman2001@hotmail.com>
2020-03-21 01:08:17 +0200

    add image on side bar

--- lib/screens/main_drawer.dart
+++ lib/screens/main_drawer.dart
@@ -4,7 +4,32 @@ class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-            child: Text("Hello World"),
-          );
+        child: Column(
+      children: <Widget>[
+        Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(20),
+          color: Theme.of(context).primaryColor,
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://simpleactivity435203168.files.wordpress.com/2019/09/flutter-1024x486.png?w=1086'),
+                              fit: BoxFit.fill,
+                              )
+                              ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
